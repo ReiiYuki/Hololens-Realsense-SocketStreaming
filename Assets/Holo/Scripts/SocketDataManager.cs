b1e9317ba8dd16b4127e58dba91fdefcfdbe6ae6
ReiiYuki <voraton.l@ku.th>
2017-06-20 17:26:29 +0900

    Prepare Update Method

---
+++ Assets/Holo/Scripts/SocketDataManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class SocketDataManager : MonoBehaviour {
+
+    SocketIOComponent socket;
+
+	// Use this for initialization
+	void Awake () {
+        socket = GetComponent<SocketIOComponent>();
+        StartCoroutine(ConnectToServer());
+        socket.On("leftHandPositionUpdate", OnLeftHandPositionUpdate);
+        socket.On("rightHandPositionUpdate", OnRightHandPositionUpdate);
+        socket.On("GestureUpdate", OnGestureUpdate);
+    }
+
+    IEnumerator ConnectToServer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["type"] = "Holo";
+        socket.Emit("identify", new JSONObject(data));
+        yield break;
+    }
+
+    void OnLeftHandPositionUpdate(SocketIOEvent evt)
+    {
+        
+    }
+
+    void OnRightHandPositionUpdate(SocketIOEvent evt)
+    {
+
+    }
+
+    void OnGestureUpdate(SocketIOEvent evt)
+    {
+
+    }
+}
